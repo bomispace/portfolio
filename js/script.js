1316097f130ab09cff4b735477f18d9e67567f68
bomispace <bomispace@naver.com>
2025-02-19 10:10:19 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const gnb_swiper = new Swiper("header .container",{
+	wrapperClass:"top_wrap",
+	slideClass:"top_box",
+	slidesPerView:"auto", //버튼의 갯수만큼 설정
+});
+
+const main_swiper = new Swiper("main",{
+	wrapperClass:"wrap",
+	slideClass:"area",
+	direction:"vertical",
+	speed:800,
+	mousewheel:{
+		enabled:true,
+	},
+	thumbs:{
+		swiper:gnb_swiper,
+		slideThumbActiveClass:"active",
+	},
+	pagination:{
+		el:".pager",
+		bulletActiveClass:'active',
+		clickable:true,
+	},
+	navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+		disabledClass:'hidden',
+  },
+});
